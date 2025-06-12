24556ac63f450e58f31a7579a6f9545d2b5b8630
ThantPyae <thantpyaes01@gmail.com>
2025-06-12 21:55:53 +0630

    set headers

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,22 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-  });
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://todo-khaki-psi.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
   console.log('Request Origin:', req.headers.origin); 
   console.log('Response Headers (after CORS):', res.getHeaders());
   next();
