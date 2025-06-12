972d9b8cfabfdf1a377256d1bf06d8fdfa0f1d3b
ThantPyae <thantpyaes01@gmail.com>
2025-06-12 22:41:32 +0630

    icon added

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
-const allowedOrigins = ["http://localhost:5173", process.env.CLIENT_ID];
+const allowedOrigins = ["http://localhost:5173", process.env.CLIENT_ID || 'https://todo-khaki-psi.vercel.app'];
 console.log('Server starting with allowedOrigins:', allowedOrigins); 
 
 // Middleware
@@ -17,27 +17,11 @@ app.use(cors({
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
+  optionsSuccessStatus: 200
 }));
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://todo-khaki-psi.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
   console.log('Request Origin:', req.headers.origin); 
   console.log('Response Headers (after CORS):', res.getHeaders());
   next();
